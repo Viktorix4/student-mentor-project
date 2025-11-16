a43a873d825446b8a54690d8a07931cf5d991d7e
Viktoria Alexeeva <alexeeva.viktori@yandex.ru>
2025-11-16 20:28:19 +0300

    Initial commit with inheritance structure

---
+++ main.py
@@ -0,0 +1,62 @@
+class Student:
+    """Класс для представления студента."""
+
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+
+class Mentor:
+    """Родительский класс для преподавателей."""
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    """Класс для лекторов — наследуется от Mentor."""
+    pass
+
+
+class Reviewer(Mentor):
+    """Класс для экспертов, проверяющих домашние задания."""
+
+    def rate_hw(self, student, course, grade):
+        if (isinstance(student, Student)
+                and course in self.courses_attached
+                and course in student.courses_in_progress):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+# Проверка наследования согласно заданию
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+
+print(isinstance(lecturer, Mentor))  # True
+print(isinstance(reviewer, Mentor))  # True
+print(lecturer.courses_attached)    # []
+print(reviewer.courses_attached)    # []
+
+# Дополнительная проверка работы Reviewer
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)  # {'Python': [10, 10, 10]}
